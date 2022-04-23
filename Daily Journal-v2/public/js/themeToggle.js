223fec0fd25be0d06f8aed1a14c485c6f1989503
EmanuelRodriguezBedeman <93904438+EmanuelRodriguezBedeman@users.noreply.github.com>
2022-04-23 16:39:49 -0300

    Add files via upload

---
+++ Daily Journal-v2/public/js/themeToggle.js
@@ -0,0 +1,6 @@
+// Changes the page between light and dark mode
+// TO DO: Add code to check the user's current o preferred theme, so the page loads on that style.
+
+function themeToggle() {
+    $("body").toggleClass("dark");
+}
