f6ffaa81616b048dd174511bba9dc5f4764fdd8e
EmanuelRodriguezBedeman <93904438+EmanuelRodriguezBedeman@users.noreply.github.com>
2022-05-06 17:49:51 -0300

    Update themeToggle.js

--- Daily Journal-v2/public/js/themeToggle.js
+++ Daily Journal-v2/public/js/themeToggle.js
@@ -1,6 +1,14 @@
 // Changes the page between light and dark mode
-// TO DO: Add code to check the user's current o preferred theme, so the page loads on that style.
+// TO DO: Add code to check the user's theme site preference. Fow now it just loads the default OS theme.
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+    $("div.ball").click();
+} else {
+    console.log("Hi!");
+}
 
 function themeToggle() {
-    $("body").toggleClass("dark");
+    $("body").toggleClass("dark-theme");
 }
