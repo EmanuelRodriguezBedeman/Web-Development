ce0bf12a57fca6f528e8c995a5fe57acc34f0ec2
EmanuelRodriguezBedeman <93904438+EmanuelRodriguezBedeman@users.noreply.github.com>
2022-05-09 08:38:13 -0300

    Update themeToggle.js

--- Daily Journal-v2/public/js/themeToggle.js
+++ Daily Journal-v2/public/js/themeToggle.js
@@ -3,12 +3,24 @@
 const osThemeDark = matchMedia("(prefers-color-scheme: dark)").matches; // Returns boolean
 const element = $("body");
 
+/* Summary:
+* Checks if the user has a theme preferrence or his OS theme
+* After that, sets the right theme for him 
+*/
+
 if (localStorage.getItem("preferredTheme") === "dark" || osTheme.matches) {
     element.addClass("dark-theme");
     moveSwitch("1.5rem");
 } else {
     localStorage.setItem("preferredTheme", "light");
 };
+
+/* Summary
+* Function for the switch button
+* Depending how the user interacts with the theme switch
+* Saves his preferrence and remembers it across the website
+* And for the next time he enters the page 
+*/
 
 function themeToggle() {
     element.addClass("transition-effect");
@@ -23,6 +35,8 @@ function themeToggle() {
     }
 };
 
+
+// Function to move the switch when loading on of the themes.
 function moveSwitch(position) {
     $("#ball").css('transform',`translateX(${position})`);
 };
