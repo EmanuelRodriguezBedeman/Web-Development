547d9dc435a3e2d9af3db7b69ab66852eda8d773
EmanuelRodriguezBedeman <93904438+EmanuelRodriguezBedeman@users.noreply.github.com>
2022-05-16 09:34:06 -0300

    Update app.js

--- Newsletter Signup/app.js
+++ Newsletter Signup/app.js
@@ -65,8 +65,6 @@ app.post("/", function (req, res) {
         console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
     }
     //Running the function and catching the errors (if any)
-    // ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LECTURE*************************
-    // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
     run().catch(e => res.sendFile(__dirname + "/failure.html"));
 });
 
